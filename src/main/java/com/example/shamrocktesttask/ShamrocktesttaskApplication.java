a1777b5c311fcb5645c7e36c67c03afd96873f07
ArthurDavlart <arthur.davletkaliyev@mail.ru>
2021-03-24 23:48:37 +0400

    Первый коммит

---
+++ src/main/java/com/example/shamrocktesttask/ShamrocktesttaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.shamrocktesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShamrocktesttaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShamrocktesttaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shamrocktesttask/ShamrocktesttaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shamrocktesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShamrocktesttaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
